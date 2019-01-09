4ef512fbf6c587ec5ffade0ebf4b684e5808bc3b
BarnettLab <46539123+BarnettLab@users.noreply.github.com>
2019-01-09 20:55:11 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,47 @@
+""" Global variables for MyelinJ_.py
+
+"""
+
+experiments = []
+CLAHEcbstate = False
+backgroundsubRolling = False
+backgroundsubNeurite = False
+myelinaverage2 = []
+neuriteaverage2 = []
+imagenames = []
+neuritedensity = []
+myelinoverlay = []
+Min = 0
+Min2 = 0
+Max = 0
+Max2 = 0
+contrast = "0.5"
+neurites = "Red", "Green", "Blue"
+SN = False
+despeckle = False
+threshChoice2 = []
+settings = ""
+userimage2 = False
+mCLAHE = False
+mCLAHE2 = False
+imageposition = 0
+imagecount = 0 
+listAllImages = []
+newusercb = False
+newuser = False
+user = ""
+multi = False
+stats = False
+subfoldernames = list()
+width1 = 500
+height1 = 200
+RscriptPath = ""
+statsfolderPath = ""
+setpixels = "0"
+threshChoice = []
+cellbodycb = False
+radius = "0"
+frangicb = False
+Sbgcbstate = False
+greyscaleMinVal = "0"
+names = ""
